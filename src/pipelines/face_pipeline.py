8c0ded2b93b15bf3c8a19aaf2b75eef36f8f58c4
Maulik Gupta <maulik.g29@gmail.com>
2026-04-27 17:53:25 +0530

    minor changes

--- src/pipelines/face_pipeline.py
+++ src/pipelines/face_pipeline.py
@@ -1,6 +1,6 @@
 import dlib
 import numpy as np
-import face_recognition
+import face_recognition_models
 from sklearn.svm import SVC
 from src.database.db import get_all_students
 import streamlit as st
